99d438ade4f1e3d76fe1017adb66b39a8b5906f7
thisisazeez <imfostersyndrome@gmail.com>
2021-10-29 19:29:52 +0100

    Patch:

---
+++ main.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            passwd = user_password
+        )
+        print("My Sql server connection Succesful!")
+    except Error as err:
+        print(f"Error: '{err}'")
+        
+    return connection
